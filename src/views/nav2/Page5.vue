09d6865138c7983222325dd3aa556f69fe16cc54
hu <hutiantian9527@gmial.com>
2020-09-01 17:11:18 +0800

    java mail发送+excel散点图

--- src/views/nav2/Page5.vue
+++ src/views/nav2/Page5.vue
@@ -1,4 +1,29 @@
 <template>
-	<section>page5...
-	</section>
+    <div>
+        <el-button type="text" @click="dialogVisible = true">点击打开 Dialog</el-button>
+
+        <el-dialog
+                title="提示"
+                :visible.sync="dialogVisible"
+                width="30%"
+                >
+            <span>这是一段信息</span>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 2消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+        </el-dialog>
+    </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                dialogVisible: false
+            };
+        },
+        methods: {
+
+        }
+    };
+</script>
